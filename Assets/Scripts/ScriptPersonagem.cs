d5b84cc45b2473ea75b31ebb30bda44adf46f7c7
DavidSchool-123 <dmpo@cesar.school>
2025-07-05 12:47:26 -0300

    Adicionando as aniamções e a lógica de movimentação

--- Assets/Scripts/ScriptPersonagem.cs
+++ Assets/Scripts/ScriptPersonagem.cs
@@ -3,22 +3,40 @@ using UnityEngine;
 public class ScriptPersonagem : MonoBehaviour
 {
     public Vector2 input;
-    public float MoveSpeed=5; 
+    public float MoveSpeed=5f;
+    private Animator animator;
+    public bool isMoving;
     void Start()
     {
         
     }
-
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
     // Update is called once per frame
     void Update()
     {
         
-        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
         if (input.x != 0)
         {
             input.y = 0;
         }
+
+        if (input != Vector2.zero)
+        {
+            isMoving = true;
+
+            animator.SetFloat("moveX", input.x);
+            animator.SetFloat("moveY", input.y);
             transform.position += (Vector3)(input * MoveSpeed * Time.deltaTime);
-        
+        }
+        else
+        {
+            isMoving = false;
+        }
+        animator.SetBool("isMoving", isMoving);
+
     }
 }
